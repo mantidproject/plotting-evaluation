1883ace6d690966695c6b2eb2ff3c388300d547a
Martyn Gigg <martyn.gigg@gmail.com>
2016-08-30 17:42:47 +0100

    Add modifiers for plot properties
    Title, X/Y Axis labels

--- qtcharts/change_plot_props/src/Figure.cpp
+++ qtcharts/change_plot_props/src/Figure.cpp
@@ -2,21 +2,22 @@
 #include <QtCharts/QLineSeries>
 #include <QtCharts/QScatterSeries>
 #include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QSpacerItem>
 
 QT_CHARTS_USE_NAMESPACE
 
 Figure::Figure(const Workspace &workspace, QWidget *parent)
     : QWidget(parent), m_chartView(new QChartView(this)),
-      m_typeBox(new QComboBox), m_curveType(1) {
+      m_propsLayout(nullptr), m_typeBox(new QComboBox), m_title(new QLineEdit),
+      m_xlabel(new QLineEdit), m_ylabel(new QLineEdit), m_curveType(1) {
   // Chart
   QChart *chart(new QChart);
   m_chartView->setChart(chart);
   chart->legend()->hide();
-  chart->setTitle("Cos(x) with errors & NaNs");
 
   // Start with a scatter series
   auto *series0 = new QScatterSeries();
-  series0->setName("Cos(x) with NaNs");
   const auto &curve0 = workspace[0];
   for (size_t i = 0; i < curve0.y.size(); ++i) {
     const auto &x = curve0.x[i];
@@ -28,24 +29,66 @@ Figure::Figure(const Workspace &workspace, QWidget *parent)
   m_chartView->chart()->axisX()->setGridLineVisible(false);
   m_chartView->chart()->axisY()->setGridLineVisible(false);
   m_chartView->chart()->axisX()->setLineVisible(true);
+  auto layout = new QHBoxLayout;
+  layout->addWidget(m_chartView);
 
+  // Properties
+  m_propsLayout = new QGridLayout();
+  m_propsLayout->addWidget(new QLabel("Title"), 0, 0);
+  m_propsLayout->addWidget(m_title, 0, 1);
+  connect(m_title, SIGNAL(textEdited(const QString &)), this,
+          SLOT(onTitleTextEdited(const QString &)));
+  m_propsLayout->addWidget(new QLabel("X Axis"), 1, 0);
+  m_propsLayout->addWidget(m_xlabel, 1, 1);
+  connect(m_xlabel, SIGNAL(textEdited(const QString &)), this,
+          SLOT(onXLabelTextEdited(const QString &)));
+  m_propsLayout->addWidget(new QLabel("Y Axis"), 2, 0);
+  m_propsLayout->addWidget(m_ylabel, 2, 1);
+  connect(m_ylabel, SIGNAL(textEdited(const QString &)), this,
+          SLOT(onYLabelTextEdited(const QString &)));
   // Curve type selection
   m_typeBox->addItem("Points");
   m_typeBox->addItem("Line");
   m_typeBox->addItem("Line + Points");
+  connect(m_typeBox, SIGNAL(currentIndexChanged(int)), this,
+          SLOT(onCurveTypeChanged(int)));
 
-  auto layout = new QHBoxLayout;
-  layout->addWidget(m_chartView);
-  layout->addWidget(m_typeBox);
+  m_propsLayout->addWidget(new QLabel("Curve Type"), 3, 0);
+  m_propsLayout->addWidget(m_typeBox, 3, 1);
+  m_propsLayout->setRowStretch(4, 1);
+
+  layout->addLayout(m_propsLayout);
   this->setLayout(layout);
 
   // signals
-  connect(m_typeBox, SIGNAL(currentIndexChanged(int)), this,
-          SLOT(onCurveTypeChanged(int)));
+}
+
+void Figure::onTitleTextEdited(const QString &newTitle) {
+  setTitleText(newTitle);
+}
+
+void Figure::onXLabelTextEdited(const QString &newLabel) {
+  setXLabelText(newLabel);
+}
+
+void Figure::onYLabelTextEdited(const QString &newLabel) {
+  setYLabelText(newLabel);
 }
 
 void Figure::onCurveTypeChanged(int newIndex) {
   switchToSeriesType(newIndex + 1);
+}
+
+void Figure::setTitleText(const QString &text) {
+  m_chartView->chart()->setTitle(text);
+}
+
+void Figure::setXLabelText(const QString &text) {
+  m_chartView->chart()->axisX()->setTitleText(text);
+}
+
+void Figure::setYLabelText(const QString &text) {
+  m_chartView->chart()->axisY()->setTitleText(text);
 }
 
 void Figure::switchToSeriesType(int type) {
--- qtcharts/change_plot_props/src/Figure.h
+++ qtcharts/change_plot_props/src/Figure.h
@@ -3,10 +3,12 @@
 
 #include "Workspace.h"
 
-#include <QWidget>
-#include <QtCharts/QAbstractSeries>
-#include <QtCharts/QChartView>
+#include <QtWidgets/QWidget>
 #include <QtWidgets/QComboBox>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QGridLayout>
+
+#include <QtCharts/QChartView>
 
 class Figure : public QWidget {
   Q_OBJECT
@@ -14,12 +16,22 @@ public:
   explicit Figure(const Workspace &workspace, QWidget *parent = 0);
 
 private slots:
+  void onTitleTextEdited(const QString &newTitle);
+  void onXLabelTextEdited(const QString &newLabel);
+  void onYLabelTextEdited(const QString &newLabel);
   void onCurveTypeChanged(int newIndex);
+
+private:
+  void setTitleText(const QString &text);
+  void setXLabelText(const QString &text);
+  void setYLabelText(const QString &text);
   void switchToSeriesType(int type);
 
 private:
   QT_CHARTS_NAMESPACE::QChartView *m_chartView;
+  QGridLayout *m_propsLayout;
   QComboBox *m_typeBox;
+  QLineEdit *m_title, *m_xlabel, *m_ylabel;
   int m_curveType;
 };
 
